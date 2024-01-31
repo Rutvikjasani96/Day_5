129215c4a220fd6b69960648170ff48ba27cdab8
Rutvik_Jasani <Rutvikjasani96@users.noreply.github.com>
2024-01-31 22:11:00 +0530

    Day 5 Updated...

--- src/PrefixSum.java
+++ src/PrefixSum.java
@@ -2,7 +2,7 @@ public class PrefixSum {
 //    Prefix Sum of the Array
     public static void main(String[] args) {
         int[] array = {1,2,3,2,3,1,4,5,2,3};
-        int ps[] = new int[10];
+        int ps[] = new int[array.length];
         prefixSum(array,ps);
         for(int i=0;i< ps.length;i++){
             System.out.print(" "+ps[i]);
